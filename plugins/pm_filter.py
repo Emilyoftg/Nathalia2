e2df315464212d3e7660efd7c6e32e272b068bc5
Emily <71280699+Emilyoftg@users.noreply.github.com>
2022-11-11 22:10:31 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -11,8 +11,6 @@ from database.connections_mdb import active_connection, all_connections, delete_
 from info import *
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram import Client, filters, enums
-from pyrogram.handlers import CallbackQueryHandler
-from pyrogram import Client, filters
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings
 from database.users_chats_db import db
